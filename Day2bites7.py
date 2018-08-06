163e5378c7d0d4e9f4e6357952c6ab6a403f83b9
Craig Cuninghame <craig3050@gmail.com>
2018-08-06 22:28:21 +0100

    Day 2
    Almost working part 1

---
+++ Day2bites7.py
@@ -0,0 +1,45 @@
+'''Extract datetimes from log entries and calculate the time
+   between the first and last shutdown events'''
+from datetime import datetime
+import os
+import urllib.request
+import re
+
+SHUTDOWN_EVENT = 'Shutdown initiated'
+
+# prep: read in the logfile
+#logfile = open('log', 'a').close()
+logfile = os.path.join('log')
+urllib.request.urlretrieve('http://bit.ly/2AKSIbf', logfile)
+
+with open(logfile) as f:
+    loglines = f.readlines()
+    print(loglines)
+
+
+def convert_to_datetime(line):
+    match = re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}', line)
+    print(match)
+    date = datetime.strptime(match.group(), '%Y-%m-%dT%H:%M:%S')
+    print(date)
+    return date
+    '''TODO 1:
+       Given a log line extract its timestamp and convert it to a datetime object. 
+       For example calling the function with:
+       INFO 2014-07-03T23:27:51 supybot Shutdown complete.
+       returns:
+       datetime(2014, 7, 3, 23, 27, 51)'''
+    pass
+
+
+def time_between_shutdowns(loglines):
+    '''TODO 2:
+       Extract shutdown events ("Shutdown initiated") from loglines and calculate the
+       timedelta between the first and last one.
+       Return this datetime.timedelta object.'''
+    pass
+
+
+for line in loglines:
+    print(line)
+    convert_to_datetime(line)
