91aa9e3c23d3c37b7025632d0a9025a5ef4b0d41
Craig Cuninghame <craig3050@gmail.com>
2018-08-13 22:23:04 +0100

    Updates, almost all complete

--- Day 7-9/Day7.py
+++ Day 7-9/Day7.py
@@ -8,23 +8,35 @@ cars = {
 
 
 def get_all_jeeps():
-    new_word = ""
-    for items in cars['Jeep']:
-        new_word += items
-        new_word += ", "
-    print(new_word)
-    return new_word
+    new_list = cars['Jeep']
+    myString = ", ".join(new_list)
+    return myString
 
     """return a comma separated string of jeep models (original order)"""
     pass
 
 
 def get_first_model_each_manufacturer():
+    myString = []
+    for item in cars:
+        myString.append((cars[item][0]))
+    return myString
+
     """return a list of matching models (original ordering)"""
     pass
 
 
 def get_all_matching_models(grep='trail'):
+    myString = []
+    for item in cars:
+        for item2 in cars[item]:
+            if grep in item2:
+                print(item2)
+                myString.append(item2)
+            print(item2)
+    print(myString)
+    return myString
+
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
@@ -36,4 +48,5 @@ def sort_car_models():
     pass
 
 get_all_jeeps()
-
+get_first_model_each_manufacturer()
+get_all_matching_models()
