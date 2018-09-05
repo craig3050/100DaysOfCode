a2ecadc9ae47eb7131b5b8d3aed493c6c114a845
Craig Cuninghame <craig3050@gmail.com>
2018-09-05 19:51:26 +0100

    Passes Bite65

--- Day19/Bite65.py
+++ Day19/Bite65.py
@@ -2,18 +2,25 @@ import itertools
 import os
 import urllib.request
 
-draw = 'W, F, G, L, B, N'
+draw = 'T, I, I, G, T, T, L'
 
-# # PREWORK
-# DICTIONARY = os.path.join('/tmp', 'dictionary.txt')
-# urllib.request.urlretrieve('http://bit.ly/2iQ3dlZ', DICTIONARY)
+# PREWORK
+DICTIONARY = os.path.join('dictionary.txt')
+urllib.request.urlretrieve('http://bit.ly/2iQ3dlZ', DICTIONARY)
+
+with open(DICTIONARY) as f:
+    dictionary = set([word.strip().lower() for word in f.read().split()])
+
+
 #
-# with open(DICTIONARY) as f:
-#     dictionary = set([word.strip().lower() for word in f.read().split()])
-#
-#
-# def get_possible_dict_words(draw):
-#     """Get all possible words from a draw (list of letters) which are
+def get_possible_dict_words(draw):
+    word_list = _get_permutations_draw(draw)
+    matched_words = []
+    for word in word_list:
+        if word.lower() in dictionary:
+            matched_words.append(word.lower())
+    print(matched_words)
+# """Get all possible words from a draw (list of letters) which are
 #        valid dictionary words. Use _get_permutations_draw and provided
 #        dictionary"""
 #     pass
@@ -30,13 +37,7 @@ def _get_permutations_draw(draw):
             for second in item:
                 temp_words += str.join(second)
             words.append(temp_words)
-    print(words)
     return words
 
 
-    return permutations
-    """Helper to get all permutations of a draw (list of letters), hint:
-       use itertools.permutations (order of letters matters)"""
-    pass
-
-_get_permutations_draw(draw)
+get_possible_dict_words(draw)
